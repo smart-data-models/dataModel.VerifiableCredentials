af1b5896e71eef9769c44a516c13de0268f25b25
Alberto Abella <alberto.abella@fiware.org>
2023-02-13 19:36:03 +0100

    test of SQL export

---
+++ NaturalPerson/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model NaturalPerson of the subject dataModel.VerifiableCredentials for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+
+CREATE TABLE NaturalPerson (credentialSchema json, credentialStatus json, credentialSubject json, evidence json, expirationDate timestamp, id text, issuanceDate timestamp, issued timestamp, issuer text, proof json, type json, validFrom timestamp, validUntil timestamp);
