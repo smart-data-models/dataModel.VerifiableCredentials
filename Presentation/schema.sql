a14ea15da8ab143d09f40b6580f859e2c8b2db3f
Alberto Abella <alberto.abella@fiware.org>
2023-02-17 00:39:37 +0100

    test of SQL export

---
+++ Presentation/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model Presentation of the subject dataModel.VerifiableCredentials for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+
+CREATE TABLE Presentation (holder text, proof json, type json, verifiableCredential json);
