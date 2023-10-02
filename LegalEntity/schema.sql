cb562a6d558aaf0ac35c4a5c5e9a89cd5769061c
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 11:06:37 +0200

    test of SQL export

--- LegalEntity/schema.sql
+++ LegalEntity/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model LegalEntity of the subject dataModel.VerifiableCredentials for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model LegalEntity of the subject dataModel.VerifiableCredentials for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 
-CREATE TABLE LegalEntity (credentialSchema json, credentialStatus json, credentialSubject json, evidence json, expirationDate timestamp, id text, issuanceDate timestamp, issued timestamp, issuer text, proof json, type json, validFrom timestamp, validUntil timestamp);
+CREATE TABLE LegalEntity (credentialSchema JSON, credentialStatus JSON, credentialSubject JSON, evidence JSON, expirationDate TIMESTAMP, id TEXT, issuanceDate TIMESTAMP, issued TIMESTAMP, issuer TEXT, proof JSON, type JSON, validFrom TIMESTAMP, validUntil TIMESTAMP);
