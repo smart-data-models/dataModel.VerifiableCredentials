1fab69875c6640603a0652688024b5583370a57a
Alberto Abella <alberto.abella@fiware.org>
2024-05-28 19:14:52 +0200

    created example of code

--- Presentation/code/code_for_using_dataModel.VerifiableCredentials_Presentation.py
+++ Presentation/code/code_for_using_dataModel.VerifiableCredentials_Presentation.py
@@ -1,5 +1,38 @@
 
-#         # This code allows you to install a orion-ld broker in a linux system
+#         # The code for installing different versions of context brokers are located after the code 
+#         
+from pysmartdatamodels import pysmartdatamodels as sdm
+import subprocess
+serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost with 1026 as default port. Edit to match your configuration
+dataModel = "Presentation"
+subject = "dataModel.VerifiableCredentials"
+holder = "did:ebsi:00012345"
+attribute = "holder"
+value = holder
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+verifiableCredential = [{'@context': ['https://www.w3.org/2018/credentials/v1'], 'id': 'urn:uuid:b5bf4778-b8f5-4d76-aa0d-aab46ae9a8fe', 'type': ['VerifiableCredential', 'VerifiableAttestation', 'VerifiableId'], 'issuer': 'did:ebsi:0001234', 'issuanceDate': '2021-11-41T00:00:00Z', 'validFrom': '2021-11-01T00:00:00Z', 'credentialSubject': {'id': 'did:ebsi:00012345', 'personalIdentifier': 'IT/DE/1234', 'familyName': 'Castafiori', 'firstName': 'Bianca', 'dateOfBirth': '1930-10-01'}, 'credentialSchema': {'id': 'https://api-test.ebsi.eu/trusted-schemas-registry/v2/schemas/0xad457662a535791e888994e97d7b5e0cdd09fbae2c8900039d2ee2d9a64969b1', 'type': 'FullJsonSchemaValidator2021'}}, {'@context': ['https://www.w3.org/2018/credentials/v1'], 'id': 'urn:uuid:8ebaad40-6b85-41d6-b7e8-86f2b5fc2b31', 'type': ['VerifiableCredential', 'VerifiableAttestation'], 'issuer': 'did:ebsi:0001234', 'issuanceDate': '2021-10-31T00:00:00Z', 'validFrom': '2021-11-01T00:00:00Z', 'credentialSubject': {'id': 'did:ebsi:00012345'}, 'credentialSchema': {'id': 'https://api-test.ebsi.eu/trusted-schemas-registry/v2/schemas/0x28d76954924d1c4747a4f1f9e3e9edc9ca965efbf8ff20e4339c2bf2323a5773', 'type': 'FullJsonSchemaValidator2021'}}]
+attribute = "verifiableCredential"
+value = verifiableCredential
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+proof = {'type': '', 'proofPurpose': '', 'created': '2021-10-31T00:00:00Z', 'verificationMethod': '', 'jws': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6ImRpZDplYnNpOnp2SFdYMzU5QTNDdmZKbkNZYUFpQWRlI0YwcjVPeXRfbGFodnZ6Nk1XbFlzM21jWU5LWmlpUWRVZnF2OHRzaEhOOXcifQ.eyJpc3MiOiJkaWQ6ZWJzaTp6dkhXWDM1OUEzQ3ZmSm5DWWFBaUFkZSIsInN1YiI6ImRpZDplYnNpOnpkUGoxR1BYamZFUlh4WFBFMVlUWWRKIiwiaWF0IjoiMjAyMC0wNi0yMlQxNDoxMTo0NFoiLCJuYmYiOiIyMDIxLTExLTAxVDAwOjAwOjAwWiIsImp0aSI6InVybjp1dWlkOjA4ZTI2ZDIyLThkY2EtNDU1OC05YzE0LTZlN2FhNzI3NWI5YiIsInZjIjp7IkBjb250ZXh0IjpbImh0dHBzOi8vd3d3LnczLm9yZy8yMDE4L2NyZWRlbnRpYWxzL3YxIl0sImlkIjoidXJuOnV1aWQ6MDhlMjZkMjItOGRjYS00NTU4LTljMTQtNmU3YWE3Mjc1YjliIiwidHlwZSI6WyJWZXJpZmlhYmxlQ3JlZGVudGlhbCIsIlZlcmlmaWFibGVBdHRlc3RhdGlvbiIsIlZlcmlmaWFibGVBdXRob3Jpc2F0aW9uIl0sImlzc3VlciI6ImRpZDplYnNpOnp2SFdYMzU5QTNDdmZKbkNZYUFpQWRlIiwiaXNzdWFuY2VEYXRlIjoiMjAyMS0xMS0wMVQwMDowMDowMFoiLCJ2YWxpZEZyb20iOiIyMDIxLTExLTAxVDAwOjAwOjAwWiIsImV4cGlyYXRpb25EYXRlIjoiMjAyNC0wNi0yMlQxNDoxMTo0NFoiLCJpc3N1ZWQiOiIyMDIwLTA2LTIyVDE0OjExOjQ0WiIsImNyZWRlbnRpYWxTdWJqZWN0Ijp7ImlkIjoiZGlkOmVic2k6emRQajFHUFhqZkVSWHhYUEUxWVRZZEoiLCJhY2Nlc3MiOlsib25ib2FyZCJdfSwiY3JlZGVudGlhbFN0YXR1cyI6eyJpZCI6Imh0dHBzOi8vYXBpLnByZXByb2QuZWJzaS5ldS90cnVzdGVkLWlzc3VlcnMtcmVnaXN0cnkvdjMvaXNzdWVycy9kaWQ6ZWJzaTp6eXJNRzhUOXhZYk5vU3d5UWE0U0dNSi9wcm94eS82Y2M1ZmNjNjIxZDgwOTg5NzdkMzc4ZWM3YmY2M2Y5YWQwYjIwNGQ1NmJiNjA0Zjc1YmM5MjkwZTcwMzNhYzg0L2NyZWRlbnRpYWxzL3N0YXR1cy81IzEyOTQzMCIsInR5cGUiOiJTdGF0dXNMaXN0MjAyMUVudHJ5Iiwic3RhdHVzUHVycG9zZSI6InJldm9jYXRpb24iLCJzdGF0dXNMaXN0SW5kZXgiOiIxMjkyMzAiLCJzdGF0dXNMaXN0Q3JlZGVudGlhbCI6Imh0dHBzOi8vYXBpLnByZXByb2QuZWJzaS5ldS90cnVzdGVkLWlzc3VlcnMtcmVnaXN0cnkvdjMvaXNzdWVycy9kaWQ6ZWJzaTp6eXJNRzhUOXhZYk5vU3d5UWE0U0dNSi9wcm94eS82Y2M1ZmNjNjIxZDgwOTg5NzdkMzc4ZWM3YmY2M2Y5YWQwYjIwNGQ1NmJiNjA0Zjc1YmM5MjkwZTcwMzNhYzg0L2NyZWRlbnRpYWxzL3N0YXR1cy81In0sImNyZWRlbnRpYWxTY2hlbWEiOnsiaWQiOiJodHRwczovL2FwaS5wcmVwcm9kLmVic2kuZXUvdHJ1c3RlZC1zY2hlbWFzLXJlZ2lzdHJ5L3YxL3NjaGVtYXMvMHg5MGJiYjVlMGViZWU2NTM4Y2RhZTQ0ZGI5YTZmOGMzOGQ4NWM1YTRkNjllOTJlMGM2MzNlYTE5ZTVhZWIxOTdmIiwidHlwZSI6IkZ1bGxKc29uU2NoZW1hVmFsaWRhdG9yMjAyMSJ9fX0.H6BFgrl8bAvGsLCk4gD12DIfUKH2W8gI2cFuRbF6eGV7fciGsr1XLxWp6jM4l9fuDXBQ6BLSVhZQR2B4uqIIrA'}
+attribute = "proof"
+value = proof
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+print(" In case you have installed the a cntext broker (see comments below )")
+print(" Execute this instruction to check that the entities has been inserted")
+command = ['curl', '-X', 'GET', 'http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000']
+result = subprocess.run(command, capture_output=True, text=True)
+print(result.stdout)
+
+#         This code allows you to install different context brokers in a linux system
+#        
+#         # ORION-LD 
 #         # The manuals are here https://github.com/FIWARE/context.Orion-LD/tree/develop/doc/manuals-ld
 #         
 #         # INSTALL NGSI LD broker (OrionLD)
@@ -23,38 +56,52 @@
 #         # Check what entities are in the broker
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
+#        # STELLIO
+#        
+#        # INSTALL NGSI LD broker (Stellio)
+#        curl -O https://raw.githubusercontent.com/stellio-hub/stellio-context-broker/develop/docker-compose.yml -O https://raw.githubusercontent.com/stellio-hub/stellio-context-broker/develop/.env
+#        curl -o config/kafka/update_run.sh --create-dirs https://raw.githubusercontent.com/stellio-hub/stellio-context-broker/develop/config/kafka/update_run.sh && chmod u+x config/kafka/update_run.sh
+#        docker compose up -d
+#        # wait for some seconds for services to be up and running
+# 
+#        # TO RELAUNCH (only if you have already installed a broker in the same machine)
+#        docker compose down
+# 
+#        # CHECK INSTANCES
+#        curl -X GET 'http://localhost:8080/actuator/health'
+#        curl -X GET 'http://localhost:8080/search-service/actuator/health'
+# 
+#        # view the logs
+#        docker-compose logs -f --tail=100
+#        
+#        # SCORPIO
+#        sudo docker pull postgis/postgis
+#        sudo docker pull scorpiobroker/all-in-one-runner:java-latest
+#        sudo docker network create fiware_default
+#        sudo docker run -d --name postgres --network=fiware_default -h postgres -p 5432 -e POSTGRES_USER=ngb -e POSTGRES_PASSWORD=ngb -e POSTGRES_DB=ngb postgis/postgis
+#        sudo docker run -d --name scorpio -h scorpio --network=fiware_default -e DBHOST=postgres -p 9090:9090  scorpiobroker/all-in-one-runner:java-latest
+#         
+#          # TO RELAUNCH (only if you have already installed a broker in the same machine)
+#        sudo docker stop scorpio
+#        sudo docker rm scorpio
+#        sudo docker stop postgres
+#        sudo docker rm postgres
+#        sudo docker network rm fiware_default
+#         
+#          # CHECK INSTANCES
+#          # Check the broker is running
+#                                # Release Info
+#        curl -X GET 'http://localhost:9090/q/info'
+#          # Health status of the broker
+#        curl -X GET 'http://localhost:9090/q/health'
+#         
+#         # Check what entities are in the broker
+#         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
+#        
+#        
 #         # now the python code you can use to insert some value in the context broker according to the data model
 #         # Version Warning! 
-#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
-#         # to work with earlier version you need to replace the import instruction for
-#         # from pysmartdatamodels import pysmartdatamodels as sdm
+#         # This code is designed to work with the version 0.8.0.1 of pysmartdatamodels or later
+# 
 #         
 #         
-import pysmartdatamodels as sdm
-import subprocess
-serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
-dataModel = "Presentation"
-subject = "dataModel.VerifiableCredentials"
-holder = "did:ebsi:00012345"
-attribute = "holder"
-value = holder
-# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
-print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
-
-verifiableCredential = [{'@context': ['https://www.w3.org/2018/credentials/v1'], 'id': 'urn:uuid:b5bf4778-b8f5-4d76-aa0d-aab46ae9a8fe', 'type': ['VerifiableCredential', 'VerifiableAttestation', 'VerifiableId'], 'issuer': 'did:ebsi:0001234', 'issuanceDate': '2021-11-41T00:00:00Z', 'validFrom': '2021-11-01T00:00:00Z', 'credentialSubject': {'id': 'did:ebsi:00012345', 'personalIdentifier': 'IT/DE/1234', 'familyName': 'Castafiori', 'firstName': 'Bianca', 'dateOfBirth': '1930-10-01'}, 'credentialSchema': {'id': 'https://api-test.ebsi.eu/trusted-schemas-registry/v2/schemas/0xad457662a535791e888994e97d7b5e0cdd09fbae2c8900039d2ee2d9a64969b1', 'type': 'FullJsonSchemaValidator2021'}}, {'@context': ['https://www.w3.org/2018/credentials/v1'], 'id': 'urn:uuid:8ebaad40-6b85-41d6-b7e8-86f2b5fc2b31', 'type': ['VerifiableCredential', 'VerifiableAttestation'], 'issuer': 'did:ebsi:0001234', 'issuanceDate': '2021-10-31T00:00:00Z', 'validFrom': '2021-11-01T00:00:00Z', 'credentialSubject': {'id': 'did:ebsi:00012345'}, 'credentialSchema': {'id': 'https://api-test.ebsi.eu/trusted-schemas-registry/v2/schemas/0x28d76954924d1c4747a4f1f9e3e9edc9ca965efbf8ff20e4339c2bf2323a5773', 'type': 'FullJsonSchemaValidator2021'}}]
-attribute = "verifiableCredential"
-value = verifiableCredential
-# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
-print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
-
-proof = {'type': '', 'proofPurpose': '', 'created': '2021-10-31T00:00:00Z', 'verificationMethod': '', 'jws': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6ImRpZDplYnNpOnp2SFdYMzU5QTNDdmZKbkNZYUFpQWRlI0YwcjVPeXRfbGFodnZ6Nk1XbFlzM21jWU5LWmlpUWRVZnF2OHRzaEhOOXcifQ.eyJpc3MiOiJkaWQ6ZWJzaTp6dkhXWDM1OUEzQ3ZmSm5DWWFBaUFkZSIsInN1YiI6ImRpZDplYnNpOnpkUGoxR1BYamZFUlh4WFBFMVlUWWRKIiwiaWF0IjoiMjAyMC0wNi0yMlQxNDoxMTo0NFoiLCJuYmYiOiIyMDIxLTExLTAxVDAwOjAwOjAwWiIsImp0aSI6InVybjp1dWlkOjA4ZTI2ZDIyLThkY2EtNDU1OC05YzE0LTZlN2FhNzI3NWI5YiIsInZjIjp7IkBjb250ZXh0IjpbImh0dHBzOi8vd3d3LnczLm9yZy8yMDE4L2NyZWRlbnRpYWxzL3YxIl0sImlkIjoidXJuOnV1aWQ6MDhlMjZkMjItOGRjYS00NTU4LTljMTQtNmU3YWE3Mjc1YjliIiwidHlwZSI6WyJWZXJpZmlhYmxlQ3JlZGVudGlhbCIsIlZlcmlmaWFibGVBdHRlc3RhdGlvbiIsIlZlcmlmaWFibGVBdXRob3Jpc2F0aW9uIl0sImlzc3VlciI6ImRpZDplYnNpOnp2SFdYMzU5QTNDdmZKbkNZYUFpQWRlIiwiaXNzdWFuY2VEYXRlIjoiMjAyMS0xMS0wMVQwMDowMDowMFoiLCJ2YWxpZEZyb20iOiIyMDIxLTExLTAxVDAwOjAwOjAwWiIsImV4cGlyYXRpb25EYXRlIjoiMjAyNC0wNi0yMlQxNDoxMTo0NFoiLCJpc3N1ZWQiOiIyMDIwLTA2LTIyVDE0OjExOjQ0WiIsImNyZWRlbnRpYWxTdWJqZWN0Ijp7ImlkIjoiZGlkOmVic2k6emRQajFHUFhqZkVSWHhYUEUxWVRZZEoiLCJhY2Nlc3MiOlsib25ib2FyZCJdfSwiY3JlZGVudGlhbFN0YXR1cyI6eyJpZCI6Imh0dHBzOi8vYXBpLnByZXByb2QuZWJzaS5ldS90cnVzdGVkLWlzc3VlcnMtcmVnaXN0cnkvdjMvaXNzdWVycy9kaWQ6ZWJzaTp6eXJNRzhUOXhZYk5vU3d5UWE0U0dNSi9wcm94eS82Y2M1ZmNjNjIxZDgwOTg5NzdkMzc4ZWM3YmY2M2Y5YWQwYjIwNGQ1NmJiNjA0Zjc1YmM5MjkwZTcwMzNhYzg0L2NyZWRlbnRpYWxzL3N0YXR1cy81IzEyOTQzMCIsInR5cGUiOiJTdGF0dXNMaXN0MjAyMUVudHJ5Iiwic3RhdHVzUHVycG9zZSI6InJldm9jYXRpb24iLCJzdGF0dXNMaXN0SW5kZXgiOiIxMjkyMzAiLCJzdGF0dXNMaXN0Q3JlZGVudGlhbCI6Imh0dHBzOi8vYXBpLnByZXByb2QuZWJzaS5ldS90cnVzdGVkLWlzc3VlcnMtcmVnaXN0cnkvdjMvaXNzdWVycy9kaWQ6ZWJzaTp6eXJNRzhUOXhZYk5vU3d5UWE0U0dNSi9wcm94eS82Y2M1ZmNjNjIxZDgwOTg5NzdkMzc4ZWM3YmY2M2Y5YWQwYjIwNGQ1NmJiNjA0Zjc1YmM5MjkwZTcwMzNhYzg0L2NyZWRlbnRpYWxzL3N0YXR1cy81In0sImNyZWRlbnRpYWxTY2hlbWEiOnsiaWQiOiJodHRwczovL2FwaS5wcmVwcm9kLmVic2kuZXUvdHJ1c3RlZC1zY2hlbWFzLXJlZ2lzdHJ5L3YxL3NjaGVtYXMvMHg5MGJiYjVlMGViZWU2NTM4Y2RhZTQ0ZGI5YTZmOGMzOGQ4NWM1YTRkNjllOTJlMGM2MzNlYTE5ZTVhZWIxOTdmIiwidHlwZSI6IkZ1bGxKc29uU2NoZW1hVmFsaWRhdG9yMjAyMSJ9fX0.H6BFgrl8bAvGsLCk4gD12DIfUKH2W8gI2cFuRbF6eGV7fciGsr1XLxWp6jM4l9fuDXBQ6BLSVhZQR2B4uqIIrA'}
-attribute = "proof"
-value = proof
-# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
-print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
-
-print(" In case you have installed the orion-ld broker (see comments on the header of this program)")
-print(" Execute this instruction to check that the entities has been inserted")
-command = ['curl', '-X', 'GET', 'http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000']
-result = subprocess.run(command, capture_output=True, text=True)
-print(result.stdout)
